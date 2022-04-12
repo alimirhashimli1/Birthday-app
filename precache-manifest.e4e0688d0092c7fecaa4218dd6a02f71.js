98a571074761663399dae1c02b8532f5a69bcc8e
alimirhashimli <alimirhashimli@gmail.com>
2022-04-12 13:14:47 +0200

    Updates

---
+++ precache-manifest.e4e0688d0092c7fecaa4218dd6a02f71.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cbac10222084ebfc49964fa840979a9f",
+    "url": "/Birthday-app/index.html"
+  },
+  {
+    "revision": "b4ededbe12669ad13dfe",
+    "url": "/Birthday-app/static/css/main.10d6a708.chunk.css"
+  },
+  {
+    "revision": "6e2d7a9fbc772ec3686f",
+    "url": "/Birthday-app/static/js/2.1f771bf1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Birthday-app/static/js/2.1f771bf1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b4ededbe12669ad13dfe",
+    "url": "/Birthday-app/static/js/main.d28b1880.chunk.js"
+  },
+  {
+    "revision": "c69f853f0f82e50e0a30",
+    "url": "/Birthday-app/static/js/runtime-main.e50e7109.js"
+  }
+]);

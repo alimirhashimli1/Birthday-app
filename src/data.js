860f79ac00dcb768772eb9c4de25408094df8dad
alimirhashimli1 <90380676+alimirhashimli1@users.noreply.github.com>
2022-04-12 13:28:28 +0200

    Update data.js

--- src/data.js
+++ src/data.js
@@ -2,42 +2,42 @@ export default [
   {
     id: 1,
     name: 'Bertie Yates',
-    birthday: '5.4.1992',
+    birthday: '12.4.1992',
     image:
       'https://res.cloudinary.com/diqqf3eq2/image/upload/v1595959131/person-2_ipcjws.jpg',
   },
   {
     id: 2,
     name: 'Hester Hogan',
-    birthday: '25.3.1942',
+    birthday: '13.4.1942',
     image:
       'https://res.cloudinary.com/diqqf3eq2/image/upload/v1595959131/person-3_rxtqvi.jpg',
   },
   {
     id: 3,
     name: 'Larry Little',
-    birthday: '23.5.1994',
+    birthday: '14.4.1994',
     image:
       'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883423/person-4_t9nxjt.jpg',
   },
   {
     id: 4,
     name: 'Sean Walsh',
-    birthday: '25.3.1998',
+    birthday: '15.4.1998',
     image:
       'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883417/person-3_ipa0mj.jpg',
   },
   {
     id: 5,
     name: 'Lola Gardner',
-    birthday: '23.3.2000',
+    birthday: '16.4.2000',
     image:
       'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883334/person-1_rfzshl.jpg',
   },
   {
     id: 6,
     name: 'Ali Mirhashimli',
-    birthday: '4.4.1992',
+    birthday: '17.4.1992',
     image:
       'https://media-exp1.licdn.com/dms/image/C4D03AQFbIw_O0tptHw/profile-displayphoto-shrink_200_200/0/1605975934164?e=2147483647&v=beta&t=Q25_BYsmDnm4E7qWJtu53y9qC6xlhnZF_dLJXcJslQU',
   },
